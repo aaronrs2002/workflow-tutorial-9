e1769ac9dc200ea2101fbcdf6e1161840020ddb3
aaronrs2002 <aaronrs2002@yahoo.com>
2024-04-18 08:37:55 -0700

    Update TicketList.js
    added uuid

--- client/src/components/TicketList.js
+++ client/src/components/TicketList.js
@@ -8,7 +8,7 @@ const TicketList = (props) => {
             <select className="form-control" name="ticketList" onChange={() => props.populateFields()}>
                 <option value="default">Select a ticket</option>
                 {props.ticketInfo !== null ? props.ticketInfo.map((ticket, i) => {
-                    return (<option key={i} data-level={ticket.priority} value={ticket.ticketId}>{ticket.ticketId.substring(ticket.ticketId.lastIndexOf(":") + 1) + " (priority: " + ticket.priority + ")"}</option>)
+                    return (<option key={i} data-level={ticket.priority} value={ticket.uuid}>{ticket.ticketId.substring(ticket.ticketId.lastIndexOf(":") + 1) + " (priority: " + ticket.priority + ")"}</option>)
                 }) : null}
             </select>
             : null

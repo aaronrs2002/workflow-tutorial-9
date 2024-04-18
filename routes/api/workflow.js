88dfedf4ac03a356b1f21853cc5c2ed57241ceca
aaronrs2002 <aaronrs2002@yahoo.com>
2024-04-18 08:32:17 -0700

    Update workflow.js
    adding uuid

--- routes/api/workflow.js
+++ routes/api/workflow.js
@@ -4,11 +4,15 @@ const db = require("../../config/db");
 
 const { checkToken } = require("../../auth/token_validation");
 
+
+
 //SERVER SIDE POST NEW STEP
 router.post("/add-workflow/", checkToken, (req, res) => {
     let sql = `INSERT INTO workflow SET ?`;
     let query = db.query(sql, {
-        ticketId: req.body.ticketId
+        ticketId: req.body.ticketId,
+        stepsData: req.body.stepsData,
+        uuid: req.body.uuid
     }, (err, result) => {
         if (err) {
             console.log("error: " + err);
@@ -20,9 +24,9 @@ router.post("/add-workflow/", checkToken, (req, res) => {
 
 
 //SERVER SIDE GET ALL STEPS
-router.get("/get-workflow/:ticketId", checkToken, (req, res) => {
+router.get("/get-workflow/:uuid", checkToken, (req, res) => {
 
-    let sql = `SELECT * FROM workflow WHERE ticketId = '${req.params.ticketId}'`;
+    let sql = `SELECT * FROM workflow WHERE uuid = '${req.params.uuid}'`;
     let query = db.query(sql, (err, result) => {
         if (err) {
             console.log("error: " + err);
@@ -35,7 +39,7 @@ router.get("/get-workflow/:ticketId", checkToken, (req, res) => {
 
 //SERVER SIDE PUT WORKFLOW STEPS
 router.put("/update-workflow/", checkToken, (req, res) => {
-    let sql = `UPDATE workflow SET stepsData = '${req.body.stepsData}' WHERE ticketId = '${req.body.ticketId}'`;
+    let sql = `UPDATE workflow SET stepsData = '${req.body.stepsData}' WHERE uuid = '${req.body.uuid}'`;
     let query = db.query(sql, (err, result) => {
         if (err) {
             console.log("error: " + err);
